9c9443f6c18ff55ed1b924336b25c0de1f054ca6
krakenrf <78108016+krakenrf@users.noreply.github.com>
2022-08-24 17:37:31 +1200

    Update heimdall_only_start.sh

--- heimdall_only_start.sh
+++ heimdall_only_start.sh
@@ -4,9 +4,8 @@
 eval "$(conda shell.bash hook)"
 conda activate kraken
 
-./kraken_doa_stop.sh
+./heimdall_only_stop.sh
 sleep 2
 
 cd heimdall_daq_fw/Firmware
-#sudo ./daq_synthetic_start.sh
 sudo env "PATH=$PATH" ./daq_start_sm.sh
